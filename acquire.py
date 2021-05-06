dc1a37ab990c19add22a1b1c99008e69d2e668bf
Lori Segovia <segovialori@gmail.com>
2021-05-06 15:05:08 -0500

    added matthews functions

--- acquire.py
+++ acquire.py
@@ -51,6 +51,83 @@ def acquire_codeup_blog():
 
 	return get_blog_articles(urls)
 
+######### Matthew's function to get all the blogs!!! ###############
+def codeup_blogs(urls):
+    '''
+    
+    Description:
+    -----------
+    This is a helper function to collect the codeup blogs
+    
+    Parameters:
+    ----------
+    urls: list
+        List of urls as strings
+    
+    '''
+    # create blank dataframe to hold results
+    blog_df = pd.DataFrame(columns=['title', 'body', 'category'])
+    
+    # loop that creates a list of urls
+    for url in urls:
+        
+        # standard lines to create a soup variable 
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        response = get(url, headers=headers)
+        html = response.text
+        soup = BeautifulSoup(html)
+        
+        # create a container
+        container = soup.select('.container')[0]
+        # within the container the h1 element = the title
+        title = container.find('h1').text
+        # collects the body text
+        body = container.find(itemprop='text').text
+        # collects the blog category
+        category = container.find('rel').text
+        # collects date
+        #date = container.find(itemprop='datePublished').text
+        # create a dictionary that holds the title and body
+        container_dict = {
+                    'title': title,
+                    'body': body,
+                    'category': category,
+                    #'date': date
+                        }
+        # converts dictionary into a dataframe
+        container_df = pd.DataFrame(container_dict,index=[0])
+        # concats the container_df to the blog_df created earlier
+        blog_df = pd.concat([blog_df, container_df], axis=0)
+    
+    # returns the blog_df with no duplicates and a reset index
+    return blog_df.drop_duplicates().reset_index(drop=True)
+
+def all_codeup_blogs():
+    '''
+    Description:
+    -----------
+    This function collects the title and body of ALL codeup blogs
+    '''
+    # hard coding the codeup blog website
+    url = 'https://codeup.com/blog/'
+    # standard lines to create a soup variable 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = get(url, headers=headers)
+    html = response.text
+    soup = BeautifulSoup(html)
+    # empty urls list
+    urls = []
+    # loop that creates the urls list
+    for a in soup.select('a.jet-listing-dynamic-link__link', href=True):
+        # add url to urls list
+        urls.append(a['href'])
+        # deletes duplicates and keeps unique values
+        urls = list(set(urls))
+    # runs the codeup_blogs with the urls created in the loop
+    blogs_df = codeup_blogs(urls)
+    # returns the blogs_df
+    return blogs_df
+
 
 
 # Inshort article
